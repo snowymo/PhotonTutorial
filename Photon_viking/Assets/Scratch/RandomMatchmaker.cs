ba2c2fe79aecf1b548a42166e10e6f7bc6259769
snowymo <snowymohehe@gmail.com>
2017-01-07 23:42:15 -0500

    archive for photon

---
+++ Photon_viking/Assets/Scratch/RandomMatchmaker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class RandomMatchmaker : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+		PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	public override void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	public override void OnJoinedRoom(){
+		GameObject monster = PhotonNetwork.Instantiate ("monsterprefab", Vector3.zero, Quaternion.identity, 0);
+		CharacterControl controller = monster.GetComponent<CharacterControl> ();
+		Debug.Log (controller.enabled);
+		controller.enabled = true;
+		CharacterCamera camera = monster.GetComponent<CharacterCamera> ();
+		Debug.Log (camera.enabled);
+		camera.enabled = true;
+		//monster.GetComponent<myThirdPersonController>().isControllable = true;
+	}
+}
